eb5f4fc6800f9234230517eecb43233850b66765
Marcelo <marcelogc2016@outlook.com>
2023-03-13 21:24:53 -0300

    meu terceiro commit

---
+++ Exe04/Program.cs
@@ -0,0 +1,19 @@
+﻿var myFloat = 10f;
+float meuFloat = 11;
+
+// Conversão de tipo numérico
+
+int myInteger = 100;
+meuFloat = (float)myInteger;
+
+decimal myDecimal = Convert.ToDecimal(myInteger);
+
+var MyDouble = 10d;
+myDecimal = 10m;
+
+string meuNumero = Convert.ToString(myInteger);
+int qtdChars = meuNumeroTexto.Lengh;
+for(int i = 0; i < qtdChars; i++)
+{
+    Console.WriteLine(meuNumeroTexto[i]);
+}
---
+++ TrabalhoNumero/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Programa {
+    static void Main(string[] args) {
+        Console.Write("Digite um número: ");
+        int valor = int.Parse(Console.ReadLine());
+        Console.WriteLine(NumeroPorExtenso(valor));
+    }
+    
+    static string NumeroPorExtenso(int valor) {
+        if (valor == 0) {
+            return "zero";
+        }
+        if (valor < 0) {
+            return "menos " + NumeroPorExtenso(-valor);
+        }
+        if (valor <= 19) {
+            string[] unidade = {
+                "um", "dois", "três", "quatro", "cinco",
+                "seis", "sete", "oito", "nove", "dez",
+                "onze", "doze", "treze", "quatorze", "quinze",
+                "dezesseis", "dezessete", "dezoito", "dezenove"
+            };
+            return unidade[valor - 1];
+        }
+        if (valor <= 99) {
+            string[] dezena = {
+                "vinte", "trinta", "quarenta", "cinquenta",
+                "sessenta", "setenta", "oitenta", "noventa"
+            };
+            string resultado = dezena[valor / 10 - 2];
+            if (valor % 10 != 0) {
+                resultado += " e " + NumeroPorExtenso(valor % 10);
+            }
+            return resultado;
+        }
+        if (valor <= 999) {
+            string resultado = NumeroPorExtenso(valor / 100) + "cento";
+            if (valor % 100 != 0) {
+                resultado += " e " + NumeroPorExtenso(valor % 100);
+            }
+            return resultado;
+        }
+        if (valor <= 999999) {
+            string resultado = NumeroPorExtenso(valor / 1000) + " mil";
+            if (valor % 1000 != 0) {
+                resultado += " e " + NumeroPorExtenso(valor % 1000);
+            }
+            return resultado;
+        }
+        if (valor <= 999999999) {
+            string resultado = NumeroPorExtenso(valor / 1000000) + " milhões";
+            if (valor % 1000000 != 0) {
+                resultado += " e " + NumeroPorExtenso(valor % 1000000);
+            }
+            return resultado;
+        }
+        return "número muito grande";
+    }
+}
